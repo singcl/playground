bbefdf2ccdf67a7e8f35117401fd9e31ddb193e3
singcl <24661881@qq.com>
2018-08-20 12:13:24 +0800

    :arrt: 数组的基本使用

--- c/main.c
+++ c/main.c
@@ -3,10 +3,12 @@
 int count;
 extern void write_extern(void);
 extern int max(int, int);
+extern int arr(void);
 
 int main()
 {
     count = 5;
     write_extern();
     printf("the max number is %d", max(12, 4));
+    arr();
 }
