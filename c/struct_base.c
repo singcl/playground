9a6995aacd5d137a58113ab7e029a0883ad5d6dc
singcl <24661881@qq.com>
2018-08-21 02:02:53 +0800

    :art: 结构体嵌套；结构体包含指向自己的指针；结构体相互包含。

--- c/struct_base.c
+++ c/struct_base.c
@@ -53,3 +53,28 @@ SIMPLE2 u1, u2[20], *u3;
 
 // 在上面的声明中，第一个和第二声明被编译器当作两个完全不同的类型，
 // 即使他们的成员列表是一样的，如果令 t3 = &s1，则是非法的。
+
+//此结构体的声明包含了其他的结构体
+struct COMPLEX
+{
+    char string[100];
+    struct SIMPLE a;
+};
+
+//此结构体的声明包含了指向自己类型的指针
+struct NODE
+{
+    char string[100];
+    struct NODE *next_node;
+};
+
+// 如果两个结构体互相包含，则需要对其中一个结构体进行不完整声明，如下所示：
+// 结构体B中包含指向结构体A的指针，在A声明完后，
+// B也随之进行声明 struct B { struct A *partner; //other members; };
+
+struct B; //对结构体B进行不完整声明
+struct A
+{
+    struct B *partner;
+    //other members;
+};
