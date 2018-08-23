3b0f4a18399067de840282b8d185ded8ccb96085
singcl <24661881@qq.com>
2018-08-23 14:58:43 +0800

    :art: 定义二叉树结构 和 二叉树先序创建函数

---
+++ b_tree/my_b_tree.c
@@ -0,0 +1,55 @@
+//    二叉树的实现（C语言）
+//    链表，递归实现
+//    编译环境：visual studio code
+//    操作系统：win7+
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+typedef char Elementtype;      // 定义数据类型，可根据需要自行定制
+typedef struct TreeNode *Node; // Node相当于 struct TreeNode *
+typedef struct TreeNode
+{
+    Elementtype Element;
+    Node left;  // 树节点的左子节点
+    Node right; // 树节点的右子节点
+} TREE, *PTREE;
+
+// 函数申明
+void CreateTree(PTREE *); // 树的先序创建函数
+
+// 主函数
+int main()
+{
+    PTREE Root;
+    printf("请先序输入二叉树的节点数据(如：QWERT******)： ");
+    CreateTree(&Root);
+
+    return 0;
+}
+
+// 定义树先序创建函数
+void CreateTree(PTREE *Root)
+{
+    char val = 0;    // 用于下面存放数据
+    val = getchar(); // 输入数据值
+    // 如果输入'*'，则指向为空
+    if (val == '*')
+        (*Root) = NULL;
+    // 如果输入非'*'，则给数据域赋值
+    else
+    {
+        (*Root) = (PTREE)malloc(sizeof(TREE)); // 申请内存空间
+        if ((*Root) == NULL)
+        {
+            printf("创建节点失败，无法分配可用内存...");
+            exit(-1);
+        }
+        else
+        {
+            (*Root)->Element = val; // 给节点数据域赋值
+            CreateTree(&(*Root)->left);
+            CreateTree(&(*Root)->right);
+        }
+    }
+}
