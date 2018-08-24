d1a801000839aa2513f8162816b873b9d911e776
singcl <24661881@qq.com>
2018-08-24 14:34:11 +0800

    :ART: 二叉树递归遍历 - 树的高度/叶子节点/叶子节点个数

--- b_tree/my_b_tree.c
+++ b_tree/my_b_tree.c
@@ -18,9 +18,13 @@ typedef struct TreeNode
 } TREE, *PTREE;
 
 // 函数申明
-void CreateTree(PTREE *); // 树的先序创建函数
-void PreOrderTree(PTREE); // 树的前序遍历函数
-void InOrderTree(PTREE);  // 树的中序遍历函数
+void CreateTree(PTREE *);  // 树的先序创建函数
+void PreOrderTree(PTREE);  // 树的前序遍历函数
+void InOrderTree(PTREE);   // 树的中序遍历函数
+void PostOrderTree(PTREE); // 树的后序遍历
+void LeafOfTree(PTREE);    // 打印树的叶子节点函数
+int Get_Leaf_Num(PTREE);   // 获取树叶子节点个数
+int Get_Height(PTREE);     // 获取树的高度
 
 // 主函数
 int main()
@@ -35,6 +39,17 @@ int main()
     printf("\n中序遍历结果为：");
     InOrderTree(Root);
 
+    printf("\n后序遍历结果为：");
+    PostOrderTree(Root);
+
+    printf("\n打印叶子节点为：");
+    LeafOfTree(Root);
+
+    printf("\n叶子节点个数为：%d", Get_Leaf_Num(Root));
+
+    printf("\n二叉树的高度为：%d", Get_Height(Root));
+
+    printf("\n");
     return 0;
 }
 
@@ -92,4 +107,62 @@ void InOrderTree(PTREE Root)
         putchar(Root->Element);
         InOrderTree(Root->right);
     }
+}
+
+// 树的后序遍历函数定义
+void PostOrderTree(PTREE Root)
+{
+    if (Root == NULL)
+        return;
+    else
+    {
+        PostOrderTree(Root->left);
+        PostOrderTree(Root->right);
+        putchar(Root->Element);
+    }
+}
+
+// 打印树的叶子节点函数定义
+void LeafOfTree(PTREE Tree)
+{
+    if (Tree == NULL)
+        return;
+    else
+    {
+        if (Tree->left == NULL && Tree->right == NULL)
+            putchar(Tree->Element);
+        else
+        {
+            LeafOfTree(Tree->left);
+            LeafOfTree(Tree->right);
+        }
+    }
+}
+
+// 获取树的叶子节点个数函数定义
+int Get_Leaf_Num(PTREE Tree)
+{
+    if (Tree == NULL)
+        return 0;
+    if (Tree->left == NULL && Tree->right == NULL)
+        return 1;
+    //递归整个树的叶子节点个数 = 左子树叶子节点的个数 + 右子树叶子节点的个数
+    return Get_Leaf_Num(Tree->left) + Get_Leaf_Num(Tree->right);
+}
+
+// 获取树的高度函数定义
+int Get_Height(PTREE Tree)
+{
+    int Height = 0;
+    if (Tree == NULL)
+        return 0;
+    // 树的高度 = max(左子树的高度，右子树的高度) + 1
+    else
+    {
+        int L_Height = Get_Height(Tree->left);
+        int R_Height = Get_Height(Tree->right);
+        Height = L_Height >= R_Height ? L_Height + 1 : R_Height + 1;
+    }
+
+    return Height;
 }
