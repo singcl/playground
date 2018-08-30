c9428f5e32e820cbf4b1861f2f36eade8da67826
singcl <24661881@qq.com>
2018-08-30 15:37:32 +0800

    :art: nodejs创建基本http服务器三种写法

---
+++ test8.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+/* // 写法一 （最常用的写法）
+var server = http.createServer((req, res) => {
+    res.end('nodejs');
+}); */
+
+/* // 写法二
+var server = new http.Server();
+server.on('request', (req, res) => {
+    res.end('nodejs');
+}); */
+
+// 写法三
+var server = new http.Server((req, res) => {
+    res.end('nodejs');
+});
+
+server.listen(4545, () => {
+    console.log('Nodejs server at port 4545');
+});
