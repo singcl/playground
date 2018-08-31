aa7f96479c0f674ff4be3cea649dcddf3c570b90
singcl <24661881@qq.com>
2018-08-31 16:09:44 +0800

    :art: 泛型约束

--- generics.ts
+++ generics.ts
@@ -26,3 +26,31 @@ function createInstance<A extends Animal>(c: new () => A): A {
 
 createInstance(Lion).keeper.nametag; // typechecks!
 createInstance(Bee).keeper.hasMask; // typechecks!
+
+// 带构造器签名的函数类型接口
+interface Ix {
+    new (x: string): number;
+}
+
+// 带构造器签名的函数类型别名 和  带构造器签名的函数类型接口一样的效果
+type xxox = new (x: string) => void;
+
+// 在TypeScript使用泛型创建工厂函数时，需要引用构造函数的类类型
+function create<T>(c: { new (): T }): T {
+    return new c();
+}
+
+// 或者这样写
+function create2<T>(c: new () => T): T {
+    return new c();
+}
+
+// 在泛型约束中使用类型参数;
+function getProperty(obj: T, key: K) {
+    return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, 'a'); // okay
+getProperty(x, 'm'); // error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd'.
