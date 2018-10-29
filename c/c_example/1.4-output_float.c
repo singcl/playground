02c0c4c24fd0b38e8fcec99972d8f8678c8d409e
singcl <24661881@qq.com>
2018-10-29 11:35:18 +0800

    :art: 使用 printf()：%f 输出浮点数，%e 输出双精度数

--- c/c_example/1.4-output_float.c
+++ c/c_example/1.4-output_float.c
@@ -4,6 +4,10 @@ int main() {
     float f; // 申明浮点数变量
     f = 12.3343434; // 定义浮点数变量
 
+    double d; // 申明双精度变量
+    d = 12.33232424; // 定义双精度变量
+
     printf("f value is: %f", f);
+    printf("double d value is %e", d);
     return 0;
 }
