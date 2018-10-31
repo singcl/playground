04a850d72c9e6ede70826719a1512236ea295683
singcl <24661881@qq.com>
2018-10-31 18:23:51 +0800

    :art: scanf() & getc() & getchar()

--- c/c_example/1.14_letter.c
+++ c/c_example/1.14_letter.c
@@ -10,7 +10,12 @@ int main()
     while (i)
     {
         printf("输入一个字母, 判断元音与辅音. 如果想退出,请输入“0”。\n");
+
+        // 所以这里会一个一个读取字符串中的字符
         scanf("%c", &cLetter);
+
+        // C 库函数 int getc(FILE * stream) 从指定的流 stream 获取下一个字符（一个无符号字符），并把位置标识符往前移动。
+        // C 库函数 int getchar(void) 从标准输入 stdin 获取一个字符（一个无符号字符）。这等同于 getc 带有 stdin 作为参数。
         getchar();
         printf("----------------------------------------------------------------------\n");
         iLetter = (int)cLetter;
