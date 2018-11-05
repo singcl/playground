ea8ed2a913ad07c3ccfd9ceaa84cd291ff63a723
singcl <24661881@qq.com>
2018-11-06 01:04:56 +0800

    :art: 判断字符是不是字母

---
+++ c/c_example/1.19_letter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("输入一个字符：");
+    scanf("%c", &c);
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        printf("%c 是字母", c);
+    }
+    else
+    {
+        printf("%c 不是字母", c);
+    }
+
+    return 0;
+}
