127bf553de1463792a4e2e1972e90f437dcc539f
singcl <24661881@qq.com>
2018-11-17 13:07:05 +0800

    :art: arithmetic

---
+++ c/c_example/1.33_arithmetic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i, j, count;
+    int start, end;
+
+    start = 2;
+    end = 10;
+
+    for (i = start; i <= end; ++i)
+    {
+        count = i;
+
+        for (j = 1; j <= 10; j++)
+        {
+            printf(" %3d", count * j);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
