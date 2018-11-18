9d65c0059d6882d988cdd7c93f0d8fa7b17b1b68
singcl <24661881@qq.com>
2018-11-18 15:53:04 +0800

    :art: 一个数分解为两个素数之和

---
+++ c/c_example/1.35_prime_number_plus.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int checkPrime(int n);
+
+int main(int argc, char const *argv[])
+{
+    int i, n, flag = 0;
+    printf("请输入正整数：");
+    scanf("%d", &n);
+    for (i = 2; i <= n / 2; ++i)
+    {
+        //检查判断
+        if (checkPrime(i) == 1)
+        {
+            if (checkPrime(n - i) == 1)
+            {
+                printf("%d = %d + %d \n", n, i, n - i);
+                flag = 1;
+            }
+        }
+    }
+
+    if (flag == 0)
+    {
+        printf("%d 不能分解为两个素数", n);
+    }
+    return 0;
+}
+
+// 判断素数
+int checkPrime(int n)
+{
+    int i, isPrime = 1;
+    for (i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+    }
+    return isPrime;
+}
