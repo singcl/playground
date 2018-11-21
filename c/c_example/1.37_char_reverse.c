d6bb9f87dd05cbe769aad7a2a43b97a00f552946
singcl <24661881@qq.com>
2018-11-22 01:07:33 +0800

    :art: 使用递归来翻转字符串

---
+++ c/c_example/1.37_char_reverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void reverseSentence();
+
+int main()
+{
+    printf("请输入一个字符串：");
+    reverseSentence();
+    return 0;
+}
+
+void reverseSentence()
+{
+    char c;
+    scanf("%c", &c);
+    if (c != '\n')
+    {
+        reverseSentence();
+        printf("%c", c);
+    }
+}
