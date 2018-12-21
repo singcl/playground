0738ea5d43df9ce11de0577e7545d292c024b0ad
singcl <1276837689@qq.com>
2018-12-21 16:58:21 +0800

    快速排序详细实现

--- c/c_sort/1.5_quick_sort.c
+++ c/c_sort/1.5_quick_sort.c
@@ -1,23 +1,19 @@
 
-/*****************************************************
-File name：quick_sort
-Author：singcl
-Version: 0.1
-Date: 2018/12/21
-Description: 对数组进行快速排序
-*****************************************************/
+/*
+ *对数组进行快速排序
+ *
+ *Author：singcl
+ *Version: 0.1
+ *Date: 2018/12/21
+*/
+
 #include <stdio.h>
 
 #define LENGTH(array) (sizeof(array)) / (sizeof(*array))
 
-/**************************************************
- *函数名：show_info
- *作用：打印数组元素
- *参数：
- *
- * array: 打印的数组
- * len - 数组元素个数
- **************************************************/
+/*
+ *打印数组元素
+ */
 void show_info(int array[], int len)
 {
     int i;
@@ -29,11 +25,9 @@ void show_info(int array[], int len)
     return;
 }
 
-/********************************
- *函数名：swap
- *作用：交换两个数的值
- *参数：int x, int y
- ********************************/
+/*
+ *交换两个数的值
+ */
 void swap(int *x, int *y)
 {
     int t;
@@ -45,12 +39,9 @@ void swap(int *x, int *y)
     return;
 }
 
-/************************************
- *函数名：quick_sort
- *作用：快速排序算法
- *参数：
- * array: 待排序数组  begin: 开始位置 end: 结束位置
- ************************************/
+/*
+ *快速排序算法
+ */
 void quick_sort(int array[], int begin, int end)
 {
     int i, j;
@@ -63,18 +54,20 @@ void quick_sort(int array[], int begin, int end)
         {
             if (array[i] > array[begin]) // 如果比较的数组元素大于基准数，则交换位置。
             {
-                swap(&array[i], &array[j]); // 交换两个数
+                // 将大数按 j 递减的方式依次放在 i 位置的后面, i 位置右边的数都大于array[begin]
+                swap(&array[i], &array[j]);
                 j--;
             }
             else
             {
-                i++; // 将数组向后移一位，继续与基准数比较
+                i++; // 自增后 i 位置的左边都是小于array[begin]的数
             }
         }
 
         /* 跳出while循环后，i = j。
-         * 此时数组被分割成两个部分  -->  array[begin+1] ~ array[i-1] < array[begin]
-         *                           -->  array[i+1] ~ array[end] > array[begin]
+         * 此时数组被分割成两个部分 ：
+         * 第一部分：array[begin+1] ~ array[i-1] < array[begin]
+         * 第二部分：array[i+1] ~ array[end] > array[begin]
          * 这个时候将数组array分成两个部分，再将array[i]与array[begin]进行比较，决定array[i]的位置。
          * 最后将array[i]与array[begin]交换，进行两个分割部分的排序！以此类推，直到最后i = j不满足条件就退出！
          */
