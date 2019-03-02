15f0a8da0ee90733ca04878b5283bba6a3f61ca7
singcl <1276837689@qq.com>
2019-03-02 11:36:37 +0800

    Rxjs V5 和 V6 使用方法有较大区别

--- rx/example1.js
+++ rx/example1.js
@@ -1,6 +1,9 @@
+// Rxjs doesn't export default, so import Rx from .. will be undefined.
+// Rxjs V5 和 V6 使用方法有较大区别
+
 import { Observable } from 'rxjs';
 
-const observable = new Observable((subscriber) => {
+const observable = Observable.create((subscriber) => {
   subscriber.next(1);
   subscriber.next(2);
   subscriber.next(3);
