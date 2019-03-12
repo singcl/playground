4e561bb1f51d2821a36d5ed08ea96aabff60a565
singcl <1276837689@qq.com>
2019-03-12 11:42:48 +0800

    :art: deno 启动一个简单的服务器

---
+++ deno/server.ts
@@ -0,0 +1,14 @@
+import { serve} from 'https://deno.land/x/std@v0.2.10/http/server.ts'
+const s = serve('0.0.0.0:8000')
+
+async function main() {
+    for await (const req of s) {
+        req.respond({
+            body: new TextEncoder().encode("Hello World\n")
+        })
+    }
+}
+
+main()
+
+// run deno server.ts start the server on port 8000
