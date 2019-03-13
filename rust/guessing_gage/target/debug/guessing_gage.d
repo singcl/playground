ef4b4da8760b37b0a9b280b1b84194feb1ab90ed
singcl <1276837689@qq.com>
2019-03-13 11:20:04 +0800

    :art: First rust example

---
+++ rust/guessing_gage/target/debug/guessing_gage.d
@@ -0,0 +1,1 @@
+C:\Users\singcl\Desktop\playground\rust\guessing_gage\target\debug\guessing_gage.pdb: C:\Users\singcl\Desktop\playground\rust\guessing_gage\src\main.rs
---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-1af6493edefb3d6b.d
@@ -0,0 +1,5 @@
+C:\Users\singcl\Desktop\playground\rust\hello_cargo\target\debug\deps\hello_cargo-1af6493edefb3d6b.exe: src\main.rs
+
+C:\Users\singcl\Desktop\playground\rust\hello_cargo\target\debug\deps\hello_cargo-1af6493edefb3d6b.d: src\main.rs
+
+src\main.rs:
