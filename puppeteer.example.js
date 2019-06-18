05ce0a83887d4dcb8be4cd0a2179df9f53e43646
singcl <1276837689@qq.com>
2019-06-18 19:23:37 +0800

    :art: 滚动截屏

--- puppeteer.example.js
+++ puppeteer.example.js
@@ -5,7 +5,7 @@ const chalk = require('chalk');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   try {
-    await page.goto('http://http://myvipgifts.com/qn/common?funid=139');
+    await page.goto('http://qu.myvipgifts.com/qn/common?funid=139');
     await page.screenshot({ path: 'imcoco.png', fullPage: true });
   } catch (error) {
     console.error(chalk.bold.underline.red(error.message));
