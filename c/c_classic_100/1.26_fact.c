8b345dc94c493b94d6d17b5f8a690cef739bdb1f
singcl <24661881@qq.com>
2019-12-28 21:26:06 +0800

    :art: 阶乘

---
+++ c/c_classic_100/1.26_fact.c
@@ -0,0 +1,29 @@
+// 题目：利用递归方法求5!。
+// 程序分析：递归公式：fn=fn_1*4!
+
+#include <stdio.h>
+
+int main(int argc, char **argv, char **env)
+{
+    int i;
+    int fact(int);
+    for (i = 0; i < 6; i++)
+    {
+        printf("%d!=%d\n", i, fact(i));
+    }
+    return 0;
+}
+
+int fact(int j)
+{
+    int sum;
+    if (j == 0)
+    {
+        sum = 1;
+    }
+    else
+    {
+        sum = j * fact(j - 1);
+    }
+    return sum;
+}
